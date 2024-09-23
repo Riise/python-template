aad67ac74faf9bce771043d7ae368fd9fd83811f
John George Riise <johngeorgeriise@gmail.com>
2024-09-23 10:23:54 +0200

    Bandit added.

--- src/app.py
+++ src/app.py
@@ -1,9 +1,22 @@
+"""App entry point.
+"""
+
+
 def main():
+    """
+    Main function that retrieves a message and prints it to the standard output.
+    """
     msg = get_message()
     print(msg)
 
 
 def get_message() -> str:
+    """
+    Returns a greeting message.
+
+    Returns:
+        str: A greeting message.
+    """
     return "Hello, World! This is a Python app running in a dev container!"
 
 
